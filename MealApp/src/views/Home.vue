782ab873a66126399aabada032a7a5cdeaab14f3
noodle-ing <nonomi2603@gmail.com>
2023-12-17 16:43:37 +0600

    Add text input for serching and filter by letter

--- MealApp/src/views/Home.vue
+++ MealApp/src/views/Home.vue
@@ -1,12 +1,20 @@
 <template>
-    <pre>{{ meals }}</pre>
+  <div class="flex flex-col p-8 ">
+        <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals">
+
+      <div class="flex justify-center gap-2 mt-2">
+        <router-link :to="{name: 'byLetter', params:{letter}}" v-for="letter of letters" :key="letter">
+          {{ letter }}
+        </router-link>
+      </div>
+  </div>
 </template>
 
 <script setup>
 import { computed, defineProps } from "vue";
 import store from "../store";
 
-const meals = computed(() => store.state.meals);
+const letters = 'ABCDEFGHIGKLMNOPQRSTUVWXYZ'.split("");
 
 // Экспортируем переменные в шаблон
 const props = defineProps(['meals']);
