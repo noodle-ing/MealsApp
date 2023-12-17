44c36ac5548d693e669b847ce3b029345a082390
noodle-ing <nonomi2603@gmail.com>
2023-12-17 15:17:27 +0600

    Create main dependencies between components

---
+++ MealApp/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <pre>{{ meals }}</pre>
+</template>
+
+<script setup>
+import { computed, defineProps } from "vue";
+import store from "../store";
+
+const meals = computed(() => store.state.meals);
+
+// Экспортируем переменные в шаблон
+const props = defineProps(['meals']);
+</script>
---
+++ MealApp/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,state.js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
